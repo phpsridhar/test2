db858d01e4ed1df943f63552378479a49996ff97
phpsridhar <phpsridhar@users.noreply.github.com>
2023-11-06 13:22:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
-def solution(letters):
-    different_letters = set()  # To store different letters that meet the conditions
-    seen_letters = set()  # To keep track of seen letters
+def solution(A):
+    N = len(A)
+    B = [0] * N
 
-    for char in letters:
-        if char.isalpha():
-            if char.lower() in seen_letters and char.isupper():
-                different_letters.add(char.lower())
-            seen_letters.add(char.lower())
+    for i in range(N):
+        if B.count(A[i]) == 0:
+            B[i] = A[i]
+        else:
+            #B[i] = A[i]-1
+            B[i] = findBigNo(B, A[i]-1)
+    return B
 
-    return len(different_letters)
+def findBigNo(B, num):
+
+    while num >=0:
+
+        if B.count(num) == 0:
+            return num
+        else:
+            num = num - 1
+            findBigNo(B, num)
+    return num
